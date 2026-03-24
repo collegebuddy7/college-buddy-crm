0e3c5cd15debc5ce8147c8a3e69ec8158d48b5d7
collegebuddy7 <collegebuddy.in@gmail.com>
2026-03-24 13:55:19 +0530

    fix runtime for prisma

--- src/app/api/admin/assign/route.ts
+++ src/app/api/admin/assign/route.ts
@@ -1,5 +1,6 @@
 export const dynamic = "force-dynamic";
 export const runtime = "nodejs"; 
+export const revalidate = 0;
 
 import { NextRequest, NextResponse } from "next/server";
 // import { prisma } from "@/lib/prisma";
